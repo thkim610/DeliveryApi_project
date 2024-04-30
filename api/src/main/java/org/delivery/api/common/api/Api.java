2c35412dc83480b8f87fb9ce951f0690be55ecec
thkim610 <thkim610@naver.com>
2024-04-30 13:52:19 +0900

    mod: 에러 처리 Api 공통 Spec 추가

--- api/src/main/java/org/delivery/api/common/api/Api.java
+++ api/src/main/java/org/delivery/api/common/api/Api.java
@@ -3,6 +3,7 @@ package org.delivery.api.common.api;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.delivery.api.common.error.ErrorCodeIfs;
 
 import javax.validation.Valid;
 
@@ -33,6 +34,30 @@ public class Api<T> {
         Api<T> api = new Api<T>();
         api.result = Result.OK();
         api.body = data;
+        return api;
+    }
+
+    public static Api<Object> ERROR(Result result){
+        Api<Object> api = new Api<Object>(); //에러가 날 경우, body에 세팅할 값이 존재하지 않음.
+        api.result = result;
+        return api;
+    }
+
+    public static Api<Object> ERROR(ErrorCodeIfs errorCodeIfs){
+        Api<Object> api = new Api<Object>();
+        api.result = Result.ERROR(errorCodeIfs);
+        return api;
+    }
+
+    public static Api<Object> ERROR(ErrorCodeIfs errorCodeIfs, Throwable tx){
+        Api<Object> api = new Api<Object>();
+        api.result = Result.ERROR(errorCodeIfs, tx);
+        return api;
+    }
+
+    public static Api<Object> ERROR(ErrorCodeIfs errorCodeIfs, String description){
+        Api<Object> api = new Api<Object>();
+        api.result = Result.ERROR(errorCodeIfs, description);
         return api;
     }
 }
--- api/src/main/java/org/delivery/api/common/api/Result.java
+++ api/src/main/java/org/delivery/api/common/api/Result.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.delivery.api.common.error.ErrorCode;
+import org.delivery.api.common.error.ErrorCodeIfs;
 
 @Data
 @NoArgsConstructor
@@ -17,9 +19,36 @@ public class Result {
 
     public static Result OK(){
         return Result.builder()
-                .resultCode(200)
-                .resultMessage("OK")
+                .resultCode(ErrorCode.OK.getErrorCode())
+                .resultMessage(ErrorCode.OK.getDescription())
                 .resultDescription("성공")
+                .build();
+    }
+
+    //에러코드 인터페이스만 받는 경우
+    public static Result ERROR(ErrorCodeIfs errorCodeIfs){
+        return Result.builder()
+                .resultCode(errorCodeIfs.getErrorCode())
+                .resultMessage(errorCodeIfs.getDescription())
+                .resultDescription("성공")
+                .build();
+    }
+
+    //서버의 스택 트레이스를 같이 내려주는 경우
+    public static Result ERROR(ErrorCodeIfs errorCodeIfs, Throwable tx){
+        return Result.builder()
+                .resultCode(errorCodeIfs.getErrorCode())
+                .resultMessage(errorCodeIfs.getDescription())
+                .resultDescription(tx.getLocalizedMessage()) //권장 X
+                .build();
+    }
+
+    //에러코드와 메시지를 담는 경우
+    public static Result ERROR(ErrorCodeIfs errorCodeIfs, String description){
+        return Result.builder()
+                .resultCode(errorCodeIfs.getErrorCode())
+                .resultMessage(errorCodeIfs.getDescription())
+                .resultDescription(description)
                 .build();
     }
 }
