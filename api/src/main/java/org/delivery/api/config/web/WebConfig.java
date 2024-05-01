54c2f334dc0ec183f0c82e379df0ab0b95c3e899
thkim610 <thkim610@naver.com>
2024-05-01 11:26:45 +0900

    feat: 사용자 인증을 위한 인터셉터 설정 - TODO : 헤더 검증 로직 필요

---
+++ api/src/main/java/org/delivery/api/config/web/WebConfig.java
@@ -0,0 +1,44 @@
+package org.delivery.api.config.web;
+
+import lombok.RequiredArgsConstructor;
+import org.delivery.api.interceptor.AuthorizationInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/**
+ * OPEN_API, DEFAULT_EXCLUDE, SWAGGER에 정의된 경로는 인터셉터를 적용하지 않음.
+ */
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthorizationInterceptor authorizationInterceptor;
+    private List<String> OPEN_API = List.of(
+            "/open-api/**"
+    );
+
+    private List<String> DEFAULT_EXCLUDE = List.of(
+            "/",
+            "favicon.ico", //아이콘 받아가는 경로
+            "/error" //에러 경로
+    );
+
+    private List<String> SWAGGER = List.of(
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/v3/api-docs/**"
+    );
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authorizationInterceptor)
+                .excludePathPatterns(OPEN_API)
+                .excludePathPatterns(DEFAULT_EXCLUDE)
+                .excludePathPatterns(SWAGGER)
+                ;
+
+    }
+}
