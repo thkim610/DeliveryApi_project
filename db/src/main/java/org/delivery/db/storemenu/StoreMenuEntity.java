bdf89cbf018b879883c3aac82fa6ef907d45c1d3
thkim610 <thkim610@naver.com>
2024-05-10 15:14:21 +0900

    feat: StoreMenu 엔티티 생성 및 레포지토리 생성

---
+++ db/src/main/java/org/delivery/db/storemenu/StoreMenuEntity.java
@@ -0,0 +1,41 @@
+package org.delivery.db.storemenu;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.delivery.db.BaseEntity;
+import org.delivery.db.store.enums.StoreStatus;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode(callSuper = true)
+@SuperBuilder
+@Entity
+@Table(name = "store_menu")
+public class StoreMenuEntity extends BaseEntity {
+    @Column(nullable = false)
+    private Long storeId;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Column(precision = 11, scale = 4, nullable = false)
+    private BigDecimal amount;
+
+    @Column(length = 50, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private StoreStatus status;
+
+    @Column(length = 200, nullable = false)
+    private String thumbnailUrl;
+
+    private int likeCount; //int인 이유 : 기본값이 0이기 때문 -> 만약 기본값이 null이면 Integer 사용
+
+    private int sequence;
+}
---
+++ db/src/main/java/org/delivery/db/storemenu/enums/StoreMenuStatus.java
@@ -0,0 +1,20 @@
+package org.delivery.db.storemenu.enums;
+
+import lombok.AllArgsConstructor;
+
+/**
+ * ENUM으로 사용하는 이유
+ * -오탈자 방지
+ * -DB로만 값을 읽더라도 알아보기 쉬움
+ * -잘못된 데이터가 삽입되는 (삽입이상) 것을 방지
+ */
+@AllArgsConstructor
+public enum StoreMenuStatus {
+
+    REGISTERED("등록"),
+    UNREGISTERED("해지"),
+    ;
+
+
+    private final String description;
+}
