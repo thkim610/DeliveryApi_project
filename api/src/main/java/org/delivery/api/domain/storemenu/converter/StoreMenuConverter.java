365cb7d39af4aed2a0c3e369db654c694a539794
thkim610 <thkim610@naver.com>
2024-05-10 15:57:22 +0900

    feat: 엔티티, DTO 변환 메뉴 컨버터 추가

---
+++ api/src/main/java/org/delivery/api/domain/storemenu/converter/StoreMenuConverter.java
@@ -0,0 +1,44 @@
+package org.delivery.api.domain.storemenu.converter;
+
+import org.delivery.api.common.annotation.Converter;
+import org.delivery.api.common.error.ErrorCode;
+import org.delivery.api.common.exception.ApiException;
+import org.delivery.api.domain.storemenu.controller.model.StoreMenuRegisterRequest;
+import org.delivery.api.domain.storemenu.controller.model.StoreMenuResponse;
+import org.delivery.db.storemenu.StoreMenuEntity;
+
+import java.util.Optional;
+
+@Converter
+public class StoreMenuConverter {
+
+    public StoreMenuEntity toEntity(StoreMenuRegisterRequest request){
+        return Optional.ofNullable(request)
+                .map(it -> {
+                    return StoreMenuEntity.builder()
+                            .storeId(request.getStoreId())
+                            .name(request.getName())
+                            .amount(request.getAmount())
+                            .thumbnailUrl(request.getThumbnailUrl())
+                            .build();
+                })
+                .orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT, "StoreRegisterRequest Null"));
+    }
+
+    public StoreMenuResponse toResponse(StoreMenuEntity storeMenuEntity){
+        return Optional.ofNullable(storeMenuEntity)
+                .map(it -> {
+                    return StoreMenuResponse.builder()
+                            .id(storeMenuEntity.getId())
+                            .storeId(storeMenuEntity.getStoreId())
+                            .name(storeMenuEntity.getName())
+                            .amount(storeMenuEntity.getAmount())
+                            .status(storeMenuEntity.getStatus())
+                            .thumbnailUrl(storeMenuEntity.getThumbnailUrl())
+                            .sequence(storeMenuEntity.getSequence())
+                            .likeCount(storeMenuEntity.getLikeCount())
+                            .build();
+                })
+                .orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT, "storeMenuEntity Null"));
+    }
+}
