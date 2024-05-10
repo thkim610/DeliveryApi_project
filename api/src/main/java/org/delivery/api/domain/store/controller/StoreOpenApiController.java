e10bd01e659480296714e51866a4b9394ce76d76
thkim610 <thkim610@naver.com>
2024-05-10 14:24:53 +0900

    feat: 비지니스 로직 추가 및 컨트롤러 생성(매장)

---
+++ api/src/main/java/org/delivery/api/domain/store/controller/StoreOpenApiController.java
@@ -0,0 +1,33 @@
+package org.delivery.api.domain.store.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.delivery.api.common.api.Api;
+import org.delivery.api.domain.store.controller.model.StoreRegisterRequest;
+import org.delivery.api.domain.store.controller.model.StoreResponse;
+import org.delivery.api.domain.user.business.StoreBusiness;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+/**
+ * 사용자 인증이 없어도 사용할 수 있는 API
+ */
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/open-api/store")
+public class StoreOpenApiController {
+
+    private final StoreBusiness storeBusiness;
+
+    //매장 등록
+    @PostMapping("/register")
+    public Api<StoreResponse> register(@Valid @RequestBody Api<StoreRegisterRequest> request){
+        StoreResponse response = storeBusiness.register(request.getBody());
+
+        return Api.OK(response);
+
+    }
+}
---
+++ api/src/main/java/org/delivery/api/domain/user/business/StoreBusiness.java
@@ -0,0 +1,41 @@
+package org.delivery.api.domain.user.business;
+
+import lombok.RequiredArgsConstructor;
+import org.delivery.api.common.annotation.Business;
+import org.delivery.api.domain.store.controller.model.StoreRegisterRequest;
+import org.delivery.api.domain.store.controller.model.StoreResponse;
+import org.delivery.api.domain.store.converter.StoreConverter;
+import org.delivery.api.domain.store.service.StoreService;
+import org.delivery.db.store.StoreEntity;
+import org.delivery.db.store.enums.StoreCategory;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Business
+@RequiredArgsConstructor
+public class StoreBusiness {
+    private final StoreService storeService;
+    private final StoreConverter storeConverter;
+
+    //매장 등록
+    public StoreResponse register(StoreRegisterRequest request){
+        //req -> entity (save) -> response
+        StoreEntity entity = storeConverter.toEntity(request);
+        StoreEntity registerEntity = storeService.register(entity);//save
+
+        return storeConverter.toResponse(registerEntity);
+    }
+
+    //카테고리 별 매장리스트 조회
+    public List<StoreResponse> searchCategory(StoreCategory storeCategory){
+        // entity list -> to response list
+        List<StoreEntity> storeList = storeService.searchByCategory(storeCategory);
+
+        return storeList.stream()
+                .map(it->{
+                    return storeConverter.toResponse(it);
+                })
+                .collect(Collectors.toList());
+    }
+}
