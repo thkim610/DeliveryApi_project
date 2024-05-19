ae239672f5f31c929a4f74205f0678738c335d85
thkim610 <thkim610@naver.com>
2024-05-19 13:19:36 +0900

    feat: 현재 진행중인 주문 내역 조회 기능 구현

--- api/src/main/java/org/delivery/api/domain/userorder/business/UserOrderBusiness.java
+++ api/src/main/java/org/delivery/api/domain/userorder/business/UserOrderBusiness.java
@@ -2,14 +2,19 @@ package org.delivery.api.domain.userorder.business;
 
 import lombok.RequiredArgsConstructor;
 import org.delivery.api.common.annotation.Business;
+import org.delivery.api.domain.store.converter.StoreConverter;
+import org.delivery.api.domain.store.service.StoreService;
+import org.delivery.api.domain.storemenu.converter.StoreMenuConverter;
 import org.delivery.api.domain.storemenu.service.StoreMenuService;
 import org.delivery.api.domain.user.model.User;
+import org.delivery.api.domain.userorder.controller.model.UserOrderDetailResponse;
 import org.delivery.api.domain.userorder.controller.model.UserOrderRequest;
 import org.delivery.api.domain.userorder.controller.model.UserOrderResponse;
 import org.delivery.api.domain.userorder.converter.UserOrderConverter;
 import org.delivery.api.domain.userorder.service.UserOrderService;
 import org.delivery.api.domain.userordermenu.converter.UserOrderMenuConverter;
 import org.delivery.api.domain.userordermenu.service.UserOrderMenuService;
+import org.delivery.db.store.StoreEntity;
 import org.delivery.db.storemenu.StoreMenuEntity;
 import org.delivery.db.userorder.UserOrderEntity;
 import org.delivery.db.userordermenu.UserOrderMenuEntity;
@@ -24,8 +29,11 @@ public class UserOrderBusiness {
     private final UserOrderService userOrderService;
     private final UserOrderConverter userOrderConverter;
     private final StoreMenuService storeMenuService;
+    private final StoreMenuConverter storeMenuConverter;
     private final UserOrderMenuConverter userOrderMenuConverter;
     private final UserOrderMenuService userOrderMenuService;
+    private final StoreService storeService;
+    private final StoreConverter storeConverter;
 
 
     /**
@@ -63,5 +71,49 @@ public class UserOrderBusiness {
 
         //응답 생성
         return userOrderConverter.toResponse(newUserOrderEntity);
+    }
+
+    //현재 진행 중인 주문 조회
+    public List<UserOrderDetailResponse> current(User user) {
+        //현재 사용자가 주문한 리스트
+        List<UserOrderEntity> UserOrderEntityList = userOrderService.current(user.getId());
+
+        //현재 주문 리스트에서 상세 주문 정보 리스트 생성
+        List<UserOrderDetailResponse> userOrderDetailResponseList = UserOrderEntityList.stream()
+                //주문 1건씩 처리
+                .map(it -> {
+                    //사용자 주문 id를 통해 사용자 주문 메뉴리스트 가져오기
+                    List<UserOrderMenuEntity> userOrderMenuList = userOrderMenuService.getUserOrderMenu(it.getId());
+                    //사용자 주문 메뉴리스트에서 가게 메뉴 상세 리스트 가져오기
+                    List<StoreMenuEntity> storeMenuList = userOrderMenuList.stream()
+                            .map(userOrderMenuEntity -> {
+
+                                StoreMenuEntity storeMenuEntity = storeMenuService.getStoreMenuWithThrow(userOrderMenuEntity.getStoreMenuId());
+
+                                return storeMenuEntity;
+                            })
+                            .collect(Collectors.toList());
+
+                    //사용자가 주문한 가게 정보 가져오기
+                    //TODO 리팩토링 필요 : get()에 대한 nullpoint 처리
+                    StoreEntity storeEntity = storeService.getStoreWithThrow(storeMenuList.stream().findFirst().get().getStoreId());
+
+                    return UserOrderDetailResponse.builder()
+                            .userOrderResponse(userOrderConverter.toResponse(it))
+                            .storeMenuResponseList(storeMenuConverter.toResponse(storeMenuList))
+                            .storeResponse(storeConverter.toResponse(storeEntity))
+                            .build();
+                }).collect(Collectors.toList());
+
+        return userOrderDetailResponseList;
+    }
+
+    //과거에 주문한 내역 조회
+    public List<UserOrderDetailResponse> history(User user) {
+
+    }
+
+    //주문 1건에 대한 조회
+    public UserOrderDetailResponse read(User user, Long orderId) {
     }
 }
